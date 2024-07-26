d111a9c4ed4ffa930e1162bdddcea0ab4af2a9b3
Yemezzy <ayubayelmee222@gmail.com>
2024-07-26 09:40:12 +0100

    first commit

---
+++ src/Home.js
@@ -0,0 +1,179 @@
+import React from 'react'
+import { FaTelegramPlane, FaTwitter } from "react-icons/fa";
+
+const Home = () => {
+  return (
+<div>
+
+<div>
+<nav className='p-5 px-10 flex justify-between border-b-2 border-black items-center'>
+  <a href=""><img src="./img/unnamed1.png" alt="" className='rounded-full w-20 h-[72px]'/></a>
+
+  <ul className='md:flex hidden w-[40%] text-lg font-semibold justify-between'>
+    <li><a href="">Home</a></li>
+    <li><a href="">About</a></li>
+    <li><a href="">Tokenomics</a></li>
+    <li><a href="">Coin Allocation</a></li>
+    <li><a href="">How to Buy</a></li>
+  </ul>
+
+
+  <div className='flex justify-center gap-5 text-3xl md:text-xl'>
+  <a href="">  <FaTelegramPlane/></a>
+   <a href=""> <FaTwitter/></a>
+</div>
+
+</nav>
+
+
+<div className='mt-20 grid md:grid-cols-2'>
+<section className='md:px-10 md:block flex justify-center'>
+  <img className='w-[80%]' src="./img/Asset 1.png" alt="" />
+</section>
+<section className='md:mt-28 mt-10 md:pr-10 md:px-0 px-5'>
+<p className='text-3xl font-bold uppercase'>Baby Carlos Coin</p>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione incidunt suscipit
+   consequuntur omnis sed fugit facilis repudiandae blanditiis assumenda porro?</p>
+
+   <div className='md:mt-10 mt-5 gap-5 flex'>
+    <button className='border-2 md:px-7 px-12 py-2 border-black hover:bg-black hover:text-white transition-all'>Buy Now</button>
+    <button className='border-2 px-12 py-2 border-black hover:bg-black hover:text-white transition-all'>Dextscreener</button>
+   </div>
+</section>
+</div>
+
+      <div>
+<div className='grid md:grid-cols-2 md:py-10 md:mt-20 mt-5 md:px-20 px-5 items-center'>
+      <section className='pt-[100px]'>
+            <p className='md:text-3xl text-xl font-bold'>ABOUT US</p>
+            <p className='md:text-xl mt-2'>
+            On June 5, 2009, The Hangover hit movie theaters across the world and proved that the 90's was 
+            a golden age for comedy movies. Now, 15 years later, I still crack up as I watch baby Carlos jacking-off at morning brunch, 
+            or Iron Mike singing Phil Collins, or Stu realizing there really is a tiger in the bathroom.
+            </p>
+            <p className='md:text-xl mt-2'>
+            We launched Baby Carlos Coin as tribute to one of the greatest comedy movies of our generation. 
+            So don't be a one man wolfpack. Join our community 
+            and start building those skittles in your satchel (or man purse!). And so we only have one question:
+            </p>
+          
+            
+            
+       
+        </section>
+     
+     <section className='flex justify-center mt-5 md:mt-0'>
+<img src="./img/unnamed.jpg" alt="" className='w-[60%]'/>
+     </section>
+  
+      </div>  
+      <p className='md:text-2xl text-xl mt-5 text-center font-semibold'>
+            "Hey Guys, You Ready To Let The Dogs Out"
+            </p>
+      </div>
+
+<div className='flex flex-col items-center mt-10'>
+  <img src="./img/unnamed3.jpg" alt="" className='md:w-[20%] w-[60%]'/>
+  <div className='flex justify-center gap-5 mt-5 text-2xl'>
+  <a href="">  <FaTelegramPlane/></a>
+   <a href=""> <FaTwitter/></a>
+</div>
+</div>
+
+      <div className='mt-20 grid md:grid-cols-2'>
+<section className='border-2 border-black mx-2'>
+        <div className='md:px-10 px-5 text-lg md:text-2xl'>
+      <p className='md:text-xl font-bold py-5'>TOKENMICS</p>
+<p>TOTAL SUPPLY: <b>1,000,000</b></p>
+<p className='mt-2 md:mt-5'>LP: <b>BURNT</b></p>
+<p className='mt-2 md:mt-5'>Contract: <b>RENOUNCED</b></p>
+<p className='mt-2 md:mt-5'>Ticker: <b>$Hangover</b></p>
+<p className='mt-2 md:mt-5'>Tax: <b>0/0</b></p>
+
+</div>
+</section>
+<section className='border-2 mt-2 md:mt-0 border-black mx-2'>
+        <div className='md:px-10 px-5 uppercase text-lg md:text-2xl'>
+      <p className='md:text-xl font-bold py-5 uppercase'>Coin Allocation</p>
+<p>Opening Liquidity:  <b>2 eth</b></p>
+<p className='mt-2 md:mt-5'>Developer Percentage: <b>2%</b></p>
+<p className='mt-2 md:mt-5'>Marketing Wallet: <b>2%</b></p>
+
+
+</div>
+</section>
+        </div>
+
+        <div className='mt-5 px-5 mx-2 border-2 border-black'>
+          <p className='text-xl py-2 text-center uppercase font-bold'>How to buy</p>
+          <div className='md:flex justify-center my-10 gap-10'>
+            <a href=""><img src="https://roaringkitty.wtf/wp-content/uploads/2023/12/uniswap.webp" alt="" className='w-20 rounded-full'/><p className='md:text-center underline'>Uniswap</p></a>
+            <a href=""><img src="https://roaringkitty.wtf/wp-content/uploads/2024/01/sushiswap-sushi-logo-1-300x300.png" alt="" className='w-20 rounded-full'/><p className='md:text-center underline'>Sushiswap</p></a>
+            <a href=""><img src="https://roaringkitty.wtf/wp-content/uploads/2024/01/icon-round-512-300x300.png" alt="" className='w-20 rounded-full'/><p className='md:text-center underline'>Pancakeswap</p></a>
+          </div>
+        </div>
+     
+</div>
+<div className='py-5 mt-10 text-center'>
+<p className='text-3xl font-bold'>
+BABY CARLOS </p>
+<p className='mt-5 text-xl'>Join our programme and follow us on X for the latest updates and a warm <br /> welcome from our friendly <br /> community...</p>
+</div>
+
+<div className='flex justify-center gap-10 text-5xl mt-50'>
+    <FaTelegramPlane/>
+    <FaTwitter/>
+</div>
+      
+
+
+      
+    
+
+       <div className='mt-20 bg-black text-center pt-10  pb-5 text-white'> 
+<p className='text-4xl'>DISCLAMER</p>
+<p className='mt-3 px-5'>$Hangover is a memecoin designed for the comedy movie loving community. 
+  It is not intended to serve as a financial investment or instrument for profit-making activities. 
+  Participants in $Hangover should understand that the memecoin may not retain any monetary value whatsoever. 
+  Additionally, the developers of this site are not affiliated with Warner Bros. 
+  Pictures, it's affiliates, or any cast or crew members of the film.</p>
+<p className='mt-3 text-2xl'>© 2024 $Hangover.  All Rights Reserved.</p>
+      </div> 
+  
+ {/* <div className='bbn w-[100vw]  h-[100vh]'>
+  <div>
+    <marquee behavior="" direction="left" className=" flex gaap-10 bg-white w-full p-2 border-b-2 border-black">
+  <section className='flex gap-10'>
+  <p>Buy</p>
+      <p>Buy</p>
+      <p>Buy</p>
+      <p>Buy</p>
+      <p>Buy</p>
+      <p>Buy</p>
+      <p>Buy</p>
+      <p>Buy</p>
+      <p>Buy</p>
+      <p>Buy</p>
+  </section>
+    </marquee>
+  </div>
+<div className='pt-20 p-10'>
+  <p className='text-3xl font-semibold'>Token Name</p>
+  <p className='w-[50%] mt-5 text-lg'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam amet neque commodi earum, ducimus eius? Blanditiis dolore est 
+  doloremque error consequuntur exercitationem quaerat amet nobis tempore, vero asperiores nemo natus?</p>
+
+  <p className='mt-5 font-semibold text-xl'>Contract Address</p>
+  <p className='text-lg'>Lorempsumdolor3883jdlkehg54jr8dlsh</p>
+  <div className='flex gap-5 mt-3'>
+    <button className='p-3 border-2 mt-2 bg-[#843722] p-2 rounded-md text-gray-200 w-[10%]'>Buy Now</button>
+    <a href=""></a>
+  </div>
+</div>
+</div>  */}
+  
+</div>
+
+  )
+}
+
+export default Home
